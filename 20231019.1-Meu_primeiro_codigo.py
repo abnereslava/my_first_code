936bd37834a2cd899b9365cbf2e759d09e6d9e5e
abnereslava <149258308+abnereslava@users.noreply.github.com>
2023-10-28 13:13:38 -0300

    Add files via upload

---
+++ 20231019.1-Meu_primeiro_codigo.py
@@ -0,0 +1,9 @@
+CorrectName = "Abner"
+
+print("Guess the name!")
+
+InputedName = input("Insira seu nome:")
+
+if InputedName == CorrectName:
+    print("Congratulations!")
+else: print("Wrong. Try again!")
